49d7f30332a6d67d7f8b44c40a3606fa9ed8961f
sotter20022541 <sotter20022541@gmail.com>
2017-10-30 21:53:53 +0700

    pop

---
+++ pop/app/testPopup.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect, url_for
+
+test = Flask(__name__)
+
+
+@test.route('/')
+def html():
+	return render_template('testPopup.html')
+
+@test.route('/getPopup', methods=['POST'])
+def getPopup():
+
+    getPopup = request.form['text1']
+
+    print(getPopup)
+test.run(debug=True)
